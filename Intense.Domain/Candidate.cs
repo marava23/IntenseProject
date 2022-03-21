9b51f505be964d375988b9efd9587dc7b9bf75e6
Miloš Maravić <90915454+marava23@users.noreply.github.com>
2022-03-21 07:49:56 +0100

    Add files via upload

---
+++ Intense.Domain/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intense.Domain
+{
+    public class Candidate : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<CandidateSkill> CandidateSkills { get; set; } = new HashSet<CandidateSkill>();
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
---
+++ Intense.Domain/CandidateSkill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intense.Domain
+{
+    public class CandidateSkill
+    {
+        public int CandidateId { get; set; }
+        public int SkillId { get; set; }
+        public virtual Candidate Candidate { get; set; }
+        public virtual Skill Skill { get; set; }
+    }
+}
